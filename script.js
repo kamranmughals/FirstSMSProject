cecad9d6244d6820e6182b93927fb9b5b20bf02e
muhammad.saqid@metriplica.com <muhammad.saqid@metriplica.com>
2021-10-01 18:38:48 +0200

    Corrections by Saqib

--- script.js
+++ script.js
@@ -6,40 +6,32 @@ let contentarea = document.querySelector(".content-area");
 let success = document.querySelector(".area-success");
 let show = document.getElementById('hide');
 let frm = document.getElementsByName('input-from')[0];
-let displayed = [];
 let data = [];
 
 function addrecords(){
-    let sname = document.getElementById('name').value;
-    let sfather = document.getElementById('father').value;
-    let sage = document.getElementById('age').value;
-    let sphone = document.getElementById('telephone').value;
-    for(let x = 0; x < data.length; x++){
-        sname += data[x];
-        sfather += data[x];
-        sage += data[x];
-        sphone += data[x];
-    }
-    let records = data;
-    if(records){
-        records = '<ul><li id="ans">'+sname+'</li><li id="ans">'+sfather+'</li><li id="ans">'+sage+'</li><li id="ans">'+sphone+'</li></ul>'
-        displayed.push(records);
-        success.innerHTML = '<h4 id="alert" > Student has been Added Successfully!! </h4>';
-    }
-    else if(records != data){
-        success.innerHTML = '<h3> Sorry Try Again!! </h3>';
-    }
+    let sname = document.getElementById('name').value || undefined;
+    let sfather = document.getElementById('father').value || undefined;
+    let sage = document.getElementById('age').value || undefined;
+    let sphone = document.getElementById('telephone').value || undefined; 
+
+    let newRecord = '<ul><li id="ans">'+sname+'</li><li id="ans">'+sfather+'</li><li id="ans">'+sage+'</li><li id="ans">'+sphone+'</li></ul>'
     frm.reset();
-    return false;
+    if (!newRecord.includes(undefined)) {
+        data.push(newRecord);
+        success.innerHTML = '<h4 id="alert" > Student has been Added Successfully!! </h4>';
+        
+    } else {
+        success.innerHTML = '<h3> Sorry Try Again!! </h3>';
+        
+    }
 }
+
+
 function showrecords(){
     show.style.display = "block";
     contentarea.innerHTML = '<span id= "border-round"class="dt">Full name</span><span class="dt">Father Name</span><span class="dt">Age</span><span id="border-round2" class="dt">Roll No</span>'
-    if(displayed.length == 0){
-    } else{
-        for (let row of displayed){
-            contentarea.innerHTML += row;
-        }
+    for (let row of data){
+        contentarea.innerHTML += row;
     }
-
+    
 }
